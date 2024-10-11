a4f4f3fe6c8992486ac29e7db3aa17a8dcbae31c
enejepromise <enejepromise@gmail.com>
2024-10-11 10:15:53 +0000

    update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,22 +1,19 @@
-#!/usr/bin/env python3
-""" 
-contains a type-annotation function of float using 'add'
 
+#!/usr/bin/env python3
 """
+Contains a type-annotated function 'add'
+"""
+
 
 def add(a: float, b: float) -> float:
     """
-    
-    Add two floats
+    Adds two floats
 
     Args:
-        a (float):first float
+        a (float): first float
         b (float): second float
-        
     Return:
         float: sum of args
-    
     """
-    
-    return a+b
+    return a + b
 
