ca0bd97749f860205df0dec4788f128547cebaf2
enejepromise <enejepromise@gmail.com>
2024-10-11 13:08:09 +0000

    mandatory task

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+annotation function floor
+"""
+
+
+def floor(n: float) -> int:
+    import math
+    """
+    Takes an argument
+
+    Args:
+        n:(float)
+
+    Returns
+        n(float)
+    """
+    return math.floor(n)
