abb6d4619c8c418dfdc9e2e1525df156156a3c93
enejepromise <enejepromise@gmail.com>
2024-10-12 00:04:16 +0000

    mandatory task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python3
+"""
+For annotated function to_str
+"""
+
+def st_str(n: float) -> str:
+    """
+    Returns the string representation of float
+    """
+    return repr(n)
