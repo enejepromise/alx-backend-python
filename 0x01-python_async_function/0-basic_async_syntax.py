2d5d380031716010396e54f764b49de683dca8f7
enejepromise <enejepromise@gmail.com>
2024-10-14 23:07:16 +0000

    mandatory task

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A Python3 module."""
+
+import asyncio
+import random
+from typing import Union
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous function that waits for a random delay and returns the delay."""
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
