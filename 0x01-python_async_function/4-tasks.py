3065edbecc68587f2234cd27b85f279bcc87f367
enejepromise <enejepromise@gmail.com>
2024-10-14 23:42:02 +0000

    mandatory

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A Python3 module for running agathering results."""
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Creates asyncio tasks and returns the sorted wait times."""
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
+
