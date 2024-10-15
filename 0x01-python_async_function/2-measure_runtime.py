5bdccd935e44b6a27e70c03c609f64c0963c1682
enejepromise <enejepromise@gmail.com>
2024-10-15 00:11:06 +0000

    update

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,6 +6,7 @@ from typing import List
 
 wait_random = __import__("0_basic_async_syntax").wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous function that returns sorted list of delays."""
     wait_times = await asyncio.gather(
